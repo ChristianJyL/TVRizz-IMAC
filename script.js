aa8f8ba014a6e86939417bbb6108df79264629d4
ChristianJyL <102579191+ChristianJyL@users.noreply.github.com>
2023-12-07 22:53:52 +0100

    add avoidance link + update script
    Script plus lisible avec sous fonction
    et cohérence entre displayPersonality et displaySerie

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
-const maxID =  70000;
+const maxID = 70000;
+const defaultText = "Unknown";
 
+/**
+ * Used to get randomly a serie or a personality
+ * @param {string} request - Request to the api (TVMaze)
+ */
 async function getRandomlyWithRequest(request) {
-    const id = Math.floor(Math.random() * maxID);
-    const reponse = await fetch(request + id);
+    const id = Math.floor(Math.random() * maxID); //get a random id between 0 and 70000
+    const reponse = await fetch(request + id); 
     const resultJson = await reponse.json();
     if (resultJson.status == 404) {
         return getRandomlyWithRequest(request);
@@ -10,74 +15,67 @@ async function getRandomlyWithRequest(request) {
     return resultJson;
 }
 
-async function getSeriesFromPersonality(id) {
-    const reponse = await fetch("https://api.tvmaze.com/people/" + id + "/castcredits?embed=show");
-    const resultJson = await reponse.json();
-    return resultJson;
+/**
+ * change attibutes for an image
+ * @param {HTMLElement} imageElement - HTMLElement corresponding to the image
+ * @param {JSON} data - JSON containing the image link 
+ */
+function setImageAttributes( imageElement, data) {
+    const imageSrc = data.image ? data.image.medium : "Image/unknownImage.png";
+    const altText = data.image ? "image of " + data.name  : "no poster";
+    imageElement.setAttribute("src", imageSrc);
+    imageElement.setAttribute("alt", altText);
+}
+
+/**
+ * change text of an HTMLElement
+ * @param {HTMLElement} Element - HTMLElement corresponding to the texxt
+ * @param {string} content - the content that we want to put
+ * @param {string} defaultText - text in case content is null
+ */
+function setTextContent(element, content, defaultText) {
+    element.innerText = content ? content : defaultText; 
+}
+
+
+
+function formatSummary(summary) { //remove <p> tag in the summary (tag present in the json by default)
+    if (summary != null) {
+        summary = summary.replace("<p>", "");
+        summary = summary.replace("</p>", "");
+        return summary;
+    }
+    return "Unknown";
 }
 
 async function displaySerie() {
     const serie = await getRandomlyWithRequest("https://api.tvmaze.com/shows/");
+    const imageElement = document.getElementById("serie-image");
 
-    const premieredText = document.getElementById("premiered");
-    premieredText.innerText = serie.premiered ? serie.premiered : "Unknown";
-
-    const genresText = document.getElementById("genres");
-    genresText.innerText = serie.genres.length !== 0 ? serie.genres : "Unknown"
-
-
-    if (serie.summary != null) {
-        serie.summary = serie.summary.replace("<p>", "");
-        serie.summary = serie.summary.replace("</p>", "");
-        
-        document.getElementById("summary").innerHTML = serie.summary;
-    }else{
-        document.getElementById("summary").innerHTML = "Unknown"
+    setImageAttributes(imageElement, serie );
+    setTextContent(document.getElementById("serie-title"), serie.name , defaultText);
+    setTextContent(document.getElementById("premiered"), serie.premiered, defaultText);
+    setTextContent(document.getElementById("language"), serie.language , defaultText);
+    if (serie.network) {
+        setTextContent(document.getElementById("network"), serie.network.name , defaultText);
     }
 
-    if (serie.language != null) {
-        document.getElementById("language").innerHTML = serie.language;
-    }else{
-        document.getElementById("language").innerHTML = "Unknown"
-    }
-
-    if (serie.image != null) {
-        document.getElementById("serie-image").setAttribute("src", serie.image.medium);
-        document.getElementById("serie-image").setAttribute("alt", "poster of " + serie.name);
-    }else{
-        document.getElementById("serie-image").setAttribute("src", "Image/unknowImage.png");
-        document.getElementById("serie-image").setAttribute("alt", "no poster");
-    }
-    if (serie.network != null) {
-        document.getElementById("network").innerHTML = serie.network.name;
-    }else{
-        document.getElementById("network").innerHTML = "Unknown"
-    }
-
-    document.getElementById("serie-title").innerHTML = serie.name;
-    
+    const genresElement = document.getElementById("genres");
+    genresElement.innerText = serie.genres.length !== 0 ? serie.genres : defaultText;
+    document.getElementById("summary").innerHTML = formatSummary(serie.summary)
 }
 
-async function displayPersonality(){
+async function displayPersonality() {
     const personality = await getRandomlyWithRequest("https://api.tvmaze.com/people/");
+    const imageElement = document.getElementById("personality-image");
 
-    document.getElementById("name").innerHTML = personality.name;
-    if (personality.birthday != null) {
-        var birthday = document.createElement("p");
-        birthday.innerHTML = "<strong>Birthday :</strong> " + personality.birthday;
-        document.getElementById("content").appendChild(birthday);
-    }
-    if (personality.country != null) {
-        var country = document.createElement("p");
-        country.innerHTML = "<strong>Country :</strong> " + personality.country.name;
-        document.getElementById("content").appendChild(country);
-    }
-    
+    setTextContent(document.getElementById("personality-name"), personality.name, defaultText);
+    setTextContent(document.getElementById("birthday"), personality.birthday, defaultText);
 
-    if (personality.image != null) {
-        document.getElementById("personality-image").setAttribute("src", personality.image.medium);
-        document.getElementById("personality-image").setAttribute("alt", "picture of " + personality.name);
+    if (personality.country) {
+        setTextContent(document.getElementById("country"),  personality.country.name, defaultText);
     }
+    setImageAttributes(imageElement,personality )
 }
 
 
